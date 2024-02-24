596608f70e18361d6ea281952d2ceec71d161683
耿 <huanggeng0607@163.com>
2024-02-24 18:23:59 +0800

    主页布局

--- app/src/main/java/com/lumostech/remotecontrol/activity/MainActivity.java
+++ app/src/main/java/com/lumostech/remotecontrol/activity/MainActivity.java
@@ -1,74 +1,93 @@
 package com.lumostech.remotecontrol.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lumostech.remotecontrol.R;
 
 import im.zego.zegoexpress.ZegoExpressEngine;
 
-public class MainActivity extends MediaProjectionActivity {
+public class MainActivity extends MediaProjectionActivity implements View.OnClickListener {
+
+    private static final String FAB_PROJECTION_TAG_CAST = "cast";
+    private static final String FAB_PROJECTION_TAG_CAST_PAUSE = "cast_pause";
+    private FloatingActionButton fabProjection;
+    private FloatingActionButton fabAssist;
+    private TextView tvCode;
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-        // 开始通话按钮
-        findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
-            // 点击开始通话
-            @Override
-            public void onClick(View view) {
-                // 创建Express SDK 实例
-                createEngine();
-                // 监听常用事件
-                setEventHandler();
-                // 登录房间
-                loginRoom("user2", "room1");
-                // 开始预览及推流
-                startPublish();
-            }
-        });
+    private void initViews() {
+        fabProjection = findViewById(R.id.fab_projection);
+        fabAssist = findViewById(R.id.fab_assist);
+        tvCode = findViewById(R.id.tv_code);
+        fabProjection.setOnClickListener(this);
+        fabAssist.setOnClickListener(this);
+        tvCode.setOnClickListener(this);
+    }
 
-        // 停止通话按钮
-        findViewById(R.id.stopButton).setOnClickListener(new View.OnClickListener() {
-            // 点击停止通话
-            @Override
-            public void onClick(View view) {
-                if(mEngine != null) {
-                    mEngine.logoutRoom();
-                    ZegoExpressEngine.destroyEngine(() -> {
-                        //销毁成功
-                    });
-                }
-            }
-        });
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_projection:
+                switchProjection();
+                break;
+            case R.id.fab_assist:
+                assist();
+            case R.id.tv_code:
+                tvCode.setInputType(InputType.TYPE_CLASS_NUMBER);
+                break;
+        }
+    }
 
-        // 停止通话按钮
-        findViewById(R.id.helpButton).setOnClickListener(new View.OnClickListener() {
-            // 点击停止通话
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RemoteControlActivity.class);
-                startActivity(intent);
+    private void switchProjection() {
+        String switchTag = fabProjection.getTag() != null ? (String) fabProjection.getTag() : "";
+        if (TextUtils.isEmpty(switchTag) || switchTag.equals(FAB_PROJECTION_TAG_CAST)) {
+            fabProjection.setImageResource(R.drawable.cast_pause);
+            fabProjection.setTag(FAB_PROJECTION_TAG_CAST_PAUSE);
+            startCast();
+        } else {
+            fabProjection.setImageResource(R.drawable.cast);
+            fabProjection.setTag(FAB_PROJECTION_TAG_CAST);
+            pauseCast();
+        }
+    }
 
-            }
-        });
+    private void startCast() {
+        // 创建Express SDK 实例
+        createEngine();
+        // 监听常用事件
+        setEventHandler();
+        // 登录房间
+        loginRoom("user2", "room1");
+        // 开始预览及推流
+        startPublish();
+    }
 
-        findViewById(R.id.testButton).setOnClickListener(new View.OnClickListener() {
-            // 点击停止通话
-            @Override
-            public void onClick(View view) {
-            }
-        });
+    private void pauseCast() {
+        if (mEngine != null) {
+            mEngine.logoutRoom();
+            ZegoExpressEngine.destroyEngine(() -> {
+                // 销毁成功
+            });
+        }
+    }
 
-        findViewById(R.id.testButton2).setOnClickListener(new View.OnClickListener() {
-            // 点击停止通话
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "click me!!!", Toast.LENGTH_SHORT).show();
-            }
-        });
+    private void assist() {
+        Intent intent = new Intent(MainActivity.this, RemoteControlActivity.class);
+        startActivity(intent);
     }
 }
