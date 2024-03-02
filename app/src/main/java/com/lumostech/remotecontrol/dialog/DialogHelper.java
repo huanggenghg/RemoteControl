94b2ef55958950fd6bf7990dff283c784b2074bb
耿 <huanggeng0607@163.com>
2024-03-02 17:41:22 +0800

    界面主题统一

--- app/src/main/java/com/lumostech/remotecontrol/dialog/DialogHelper.java
+++ app/src/main/java/com/lumostech/remotecontrol/dialog/DialogHelper.java
@@ -1,13 +1,14 @@
 package com.lumostech.remotecontrol.dialog;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class DialogHelper {
     public static Dialog showMessagePositiveDialog(Activity activity,String title,String msg) {
-       return new AlertDialog.Builder(activity)
+        return new MaterialAlertDialogBuilder(activity)
                 .setTitle(title)
                 .setMessage(msg)
                .setCancelable(false)
